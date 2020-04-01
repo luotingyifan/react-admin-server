9d10624639e6424b88df6a732ae93b7cea179fd4
luoting <luoting@mcpayment.net>
2020-04-01 10:23:01 +0800

    init app server

---
+++ src/test/java/com/react/admin/ReactAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.react.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactAdminServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
